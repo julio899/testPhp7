cfd0ad27a1e566e00fa85b0c5b5e49b76c9faa14
Julio Vinachi <julio899@gmail.com>
2019-07-13 12:55:04 -0400

    add redirect to / and login

---
+++ app/controllers/Session.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\controllers;
+
+/**
+ * Controllers Session
+ */
+class Session{
+	var $uri_request = [];
+	var $uri_permitted = [];
+	var $permitted = false;
+	function __construct()
+	{
+		@session_start();
+		$this->uri_permitted = unserialize (URIS);
+		$_SESSION['uri'] = $this->process_url_request();
+
+		$this->permitted = $this->url_exist_in_valids();
+		# force to update always
+		$_SESSION['permitted'] = $this->permitted;
+
+		if (!isset($_SESSION['Start'])) {
+			$_SESSION['Start'] = date("Y-m-d h:i:s",time());
+		}
+
+		if (!isset($_SESSION['log'])) {
+			$_SESSION['log'] = 0;
+		}
+
+		
+	}
+
+	public function url_exist_in_valids(): bool {
+		$is = false;
+		if(count($_SESSION['uri']) == 1)
+		{
+			# if == 1 is because is in root
+			$is = true;
+			$_SESSION['page'] = '/';
+		}else{
+
+			foreach ($this->uri_permitted as $key => $value) {
+				
+				foreach ($_SESSION['uri'] as $k => $v) {
+					if ( $value === $v ){
+						$is = true;
+					}
+				}
+			}
+			$_SESSION['page'] = $_SESSION['uri'][1];
+		}
+
+		return $is;
+	}
+
+	public function process_url_request(): array
+	{
+
+		$uri = urldecode(
+		    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+		);
+
+		$parts_url = explode('/', $uri);
+		$arr_unique = array_unique($parts_url);
+		
+		$parts_clean = [];
+
+		foreach ($arr_unique as $key => $value) {
+			if( $value !== "" )
+				array_push($parts_clean,$value);
+		}
+		return $parts_clean;
+	}
+}
---
+++ src/views/Login.php
@@ -0,0 +1,26 @@
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5 custom_box_login">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form autocomplete="off" class="form-signin">
+              <div class="form-label-group">
+                <input autocomplete="false" type="email" id="UserLogin" class="form-control" placeholder="Email address" required autofocus>
+                <label for="UserLogin">Email address</label>
+              </div>
+
+              <div class="form-label-group">
+                <input autocomplete="false" type="password" id="PasswordLogin" class="form-control" placeholder="Password" required>
+                <label for="PasswordLogin">Password</label>
+              </div>
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+              <hr class="my-4">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
