1cdaa691d66f0b0e11e3bc999946f35fd72cc64c
Julio Vinachi <julio899@gmail.com>
2019-07-14 20:20:15 -0400

    By Julio Vinachi

--- src/views/LandingPage.php
+++ src/views/LandingPage.php
@@ -89,7 +89,7 @@
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+      <p class="m-0 text-center text-white">By Julio Vinachi 2019</p>
     </div>
     <!-- /.container -->
   </footer>
