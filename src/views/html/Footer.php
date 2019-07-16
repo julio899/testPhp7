1c35dd5847633c85660ef445767a11e2450f6564
Julio Vinachi <julio899@gmail.com>
2019-07-15 23:57:44 -0400

    update order footer

--- src/views/html/Footer.php
+++ src/views/html/Footer.php
@@ -1,8 +1,8 @@
 
 <?php if ($_SESSION['page'] != 'login'): ?>
 <script src="<?php echo URL_HOST; ?>src/assets/js/jquery-3.3.1.slim.min.js"></script>
-<script src="<?php echo URL_HOST; ?>src/assets/js/bootstrap.min.js"></script>
 <script src="<?php echo URL_HOST; ?>src/assets/js/popper.min.js"></script>
+<script src="<?php echo URL_HOST; ?>src/assets/js/bootstrap.min.js"></script>
 <script src="<?php echo URL_HOST; ?>src/assets/js/alertify.min.js"></script>
 <script src="<?php echo URL_HOST; ?>src/assets/js/all.min.js"></script>
 <script src="<?php echo URL_HOST; ?>src/assets/js/custom.js"></script>
